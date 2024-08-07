98f8ae08c296ca154a0236f4067e3fa620f8f217
Pancakeboy1987 <zane41181@gmail.com>
2024-08-07 13:39:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
-TOKEN ='7489927664:AAGZFagSdY_pQuZ7KOGsh76F2F-g4UO95QU'
+
 
 #user_id = 503352199
 bot = Bot(token=TOKEN, default=DefaultBotProperties(
